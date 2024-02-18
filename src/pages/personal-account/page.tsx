e4ad2fdf6afe87d707aeb54d7a1c36ad3b7eb702
Kirill <93765186+Arsaide@users.noreply.github.com>
2024-02-18 22:52:44 +0200

    - Added logic for the "Registration/Login" btns for a personal acc on smartphones

--- src/pages/personal-account/page.tsx
+++ src/pages/personal-account/page.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useContext } from 'react';
+import React, { FC, useContext, useState } from 'react';
 import PersonalData from '../../components/pages/personalAccount/personalData/PersonalData';
 import LoginForm from '../../components/pages/auth/loginForm/LoginForm';
 import Typography from '@mui/material/Typography';
@@ -6,40 +6,71 @@ import { Context } from '../../index';
 import Button from '@mui/material/Button';
 import Avatar from '@mui/material/Avatar';
 import { Box } from '@mui/material';
+import RegistrationForm from '../../components/pages/auth/regestrationForm/RegistrationForm';
 
 const PersonalAccount: FC = () => {
     const { store } = useContext(Context);
     const isAuth = localStorage.getItem('isAuth') === 'true';
+    const [showLoginComponent, setShowLoginComponent] =
+        useState<boolean>(false);
+    const [showRegComponent, setShowRegComponent] = useState<boolean>(false);
+
     const handleSubmit = () => {
         store.logout();
         window.location.reload();
     };
 
+    const handleClickShowLogin = () => {
+        setShowLoginComponent(true);
+        setShowRegComponent(false);
+    };
+    const handleClickShowReg = () => {
+        setShowRegComponent(true);
+        setShowLoginComponent(false);
+    };
+
     return (
         <section>
-            <Typography sx={{ mb: 3 }} variant={'h4'}>
-                Personal Account
-            </Typography>
             {isAuth ? (
-                <Box>
-                    <Box sx={{ display: 'flex', gap: 4, flexWrap: 'wrap' }}>
-                        <Avatar
-                            alt="Monkey King"
-                            src="/avatar/avatar.jpg"
-                            sx={{ width: 106, height: 106 }}
-                        />
-                        <PersonalData />
+                <>
+                    <Typography sx={{ mb: 3 }} variant={'h4'}>
+                        Personal Account
+                    </Typography>
+                    <Box>
+                        <Box sx={{ display: 'flex', gap: 4, flexWrap: 'wrap' }}>
+                            <Avatar
+                                alt="Monkey King"
+                                src="/avatar/avatar.jpg"
+                                sx={{ width: 106, height: 106 }}
+                            />
+                            <PersonalData />
+                        </Box>
+                        <Button
+                            variant="contained"
+                            onClick={handleSubmit}
+                            sx={{ mt: 3 }}
+                        >
+                            Log out
+                        </Button>
                     </Box>
-                    <Button
-                        variant="contained"
-                        onClick={handleSubmit}
-                        sx={{ mt: 3 }}
-                    >
-                        Log out
-                    </Button>
-                </Box>
+                </>
             ) : (
-                <LoginForm />
+                <>
+                    {showLoginComponent || showRegComponent ? null : (
+                        <>
+                            <Box sx={{ display: 'flex' }}>
+                                <Button onClick={handleClickShowLogin}>
+                                    Log-in
+                                </Button>
+                                <Button onClick={handleClickShowReg}>
+                                    Registration
+                                </Button>
+                            </Box>
+                        </>
+                    )}
+                    {showLoginComponent && <LoginForm />}
+                    {showRegComponent && <RegistrationForm />}
+                </>
             )}
         </section>
     );
