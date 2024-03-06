d959925ba16aaba60578f194524115e19e2048a3
Kirill <93765186+Arsaide@users.noreply.github.com>
2024-03-07 01:49:08 +0200

    - added DateTable own auctions

--- src/components/pages/personal-account/user/ownAuctionsList/OwnAuctionsList.tsx
+++ src/components/pages/personal-account/user/ownAuctionsList/OwnAuctionsList.tsx
@@ -1,17 +1,91 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { AuctionInt } from '../../../../../app/rate/rate-id/AuctionItemInt';
+import Box from '@mui/material/Box';
+import { DataGrid, GridCellParams, GridColDef } from '@mui/x-data-grid';
+import clsx from 'clsx';
+import './OwnAuctionsList.css';
+import { Link } from 'react-router-dom';
+import dayjs from 'dayjs';
+
+const formatDate = (dateString: string) => {
+    const date = dayjs(dateString);
+    return date.format('DD.MM.YYYY');
+};
 
 const OwnAuctionsList: FC<{ auctions: AuctionInt[] }> = ({ auctions }) => {
+    const [responsive, setResponsive] = useState('vertical');
+    const [tableBodyHeight, setTableBodyHeight] = useState('400px');
+    const [tableBodyMaxHeight, setTableBodyMaxHeight] = useState('');
+
+    const columns: GridColDef[] = [
+        {
+            field: '_id',
+            headerName: 'ID',
+            width: 225,
+            renderCell: (params: GridCellParams) => (
+                <Link
+                    to={`/rate/${String(params.value)}`}
+                    className="white-link"
+                >
+                    {String(params.value)}
+                </Link>
+            ),
+        },
+        { field: 'title', headerName: 'Title', width: 250 },
+        { field: 'minRates', headerName: 'Minimum Rate', width: 200 },
+        { field: 'desct', headerName: 'Description', width: 200 },
+        { field: 'active', headerName: 'Active', width: 100 },
+        {
+            field: 'timeStart',
+            headerName: 'Time Start',
+            width: 150,
+            renderCell: (params: GridCellParams) => (
+                <div>{formatDate(String(params.value))}</div>
+            ),
+        },
+        {
+            field: 'timeEnd',
+            headerName: 'End Time',
+            width: 150,
+            renderCell: (params: GridCellParams) => (
+                <div>{formatDate(String(params.value))}</div>
+            ),
+        },
+    ].map(column => ({
+        ...column,
+        headerClassName: 'custom-header-color-class',
+    }));
+
+    const rows = auctions.map(item => ({
+        ...item,
+        id: item._id,
+    }));
+
     return (
         <>
-            Own Auctions:
-            <ul>
-                {auctions.map(item => (
-                    <li key={item._id}>
-                        <a href={`/rate/${item._id}`}>{item.title}</a>
-                    </li>
-                ))}
-            </ul>
+            <div style={{ width: '100%' }}>
+                <Box sx={{ height: 371, width: '100%' }}>
+                    <DataGrid
+                        rows={rows}
+                        columns={columns}
+                        initialState={{
+                            pagination: {
+                                paginationModel: {
+                                    pageSize: 5,
+                                },
+                            },
+                        }}
+                        getCellClassName={params =>
+                            clsx('custom-text-color-class', {
+                                'your-condition-class':
+                                    params.value === 'someValue',
+                            })
+                        }
+                        className="custom-pagination-color"
+                        pageSizeOptions={[5]}
+                    />
+                </Box>
+            </div>
         </>
     );
 };
