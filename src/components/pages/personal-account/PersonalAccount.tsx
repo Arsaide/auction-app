332d47f41cd40caddbc8a05ea87037dd004c0c5f
Kirill <93765186+Arsaide@users.noreply.github.com>
2024-03-06 23:58:57 +0200

    - Distributed into the components of the page "Personal Account", "rateId" "UserId"

---
+++ src/components/pages/personal-account/PersonalAccount.tsx
@@ -0,0 +1,107 @@
+import React, { FC, useContext, useEffect, useState } from 'react';
+import { Context } from '../../../index';
+import { useNavigate } from 'react-router-dom';
+import { Box, Hidden } from '@mui/material';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import LoginForm from '../auth/loginForm/LoginForm';
+import RegistrationForm from '../auth/regestrationForm/RegistrationForm';
+
+const PersonalAccountPage: FC = () => {
+    const { store } = useContext(Context);
+    const [showLoginComponent, setShowLoginComponent] =
+        useState<boolean>(false);
+    const [showRegComponent, setShowRegComponent] = useState<boolean>(false);
+    const navigate = useNavigate();
+    const isAuth = localStorage.getItem('isAuth') === 'true';
+    const token = localStorage.getItem('token');
+
+    const redirectPage = () => {
+        if (isAuth) {
+            navigate(`/personal-account/${token}`);
+        }
+    };
+
+    useEffect(() => {
+        redirectPage();
+    }, []);
+
+    const handleClickShowLogin = () => {
+        setShowLoginComponent(true);
+        setShowRegComponent(false);
+    };
+    const handleClickShowReg = () => {
+        setShowRegComponent(true);
+        setShowLoginComponent(false);
+    };
+
+    return (
+        <>
+            {showLoginComponent || showRegComponent ? null : (
+                <>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            justifyContent: 'center',
+                            gap: 3,
+                        }}
+                    >
+                        <Hidden mdUp>
+                            <img
+                                style={{ width: '100%' }}
+                                src="/flr2.gif"
+                                alt="Тут будет логотип в будущем"
+                            />
+                        </Hidden>
+                        <Typography variant={'h5'} sx={{ textAlign: 'center' }}>
+                            To continue please log in.
+                        </Typography>
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                gap: 3,
+                            }}
+                        >
+                            <Button
+                                color="success"
+                                variant="contained"
+                                size="large"
+                                onClick={handleClickShowLogin}
+                            >
+                                Log-in
+                            </Button>
+                            <Button
+                                color="success"
+                                variant="contained"
+                                size="large"
+                                onClick={handleClickShowReg}
+                            >
+                                Registration
+                            </Button>
+                        </Box>
+                    </Box>
+                </>
+            )}
+            {showLoginComponent && (
+                <>
+                    <Typography variant={'h5'} sx={{ textAlign: 'center' }}>
+                        To continue please log in.
+                    </Typography>
+                    <LoginForm />
+                </>
+            )}
+            {showRegComponent && (
+                <>
+                    <Typography variant={'h5'} sx={{ textAlign: 'center' }}>
+                        To continue please log in.
+                    </Typography>
+                    <RegistrationForm />
+                </>
+            )}
+        </>
+    );
+};
+
+export default PersonalAccountPage;
---
+++ src/components/pages/personal-account/user/UserIdPage.tsx
@@ -0,0 +1,80 @@
+import React, { FC, useContext, useEffect, useState } from 'react';
+import { Context } from '../../../../index';
+import { useNavigate, useParams } from 'react-router-dom';
+import Grid from '@mui/material/Grid';
+import CircularProgress from '@mui/material/CircularProgress';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { AuctionInt } from '../../../../app/rate/rate-id/AuctionItemInt';
+import { UserIdInt } from '../../../../app/personal-account/user-id/UserInterface';
+
+const UserIdPage: FC = () => {
+    const { store } = useContext(Context);
+    const { token } = useParams<{ token: string }>();
+    const [user, setUser] = useState<UserIdInt | null>(null);
+    const navigate = useNavigate();
+    const isAuth = localStorage.getItem('isAuth') === 'true';
+    const [auctions, setAuctions] = useState<AuctionInt[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [isRequesting, setIsRequesting] = useState<boolean>(false);
+
+    const redirectPage = () => {
+        if (!isAuth) {
+            navigate(`/personal-account`);
+        }
+    };
+
+    useEffect(() => {
+        redirectPage();
+    }, []);
+
+    useEffect(() => {
+        const fetchAccount = async () => {
+            try {
+                const response = await store.getUser(token);
+                const auctionResponse = await store.getOwnAuctions(token);
+                console.log(auctionResponse.data.auctions);
+                setUser(response.data.user as unknown as UserIdInt);
+                setAuctions(auctionResponse.data.auctions);
+            } catch (error) {
+                console.error('Error fetching account:', error);
+            }
+        };
+
+        fetchAccount();
+    }, [store, token]);
+
+    const handleSubmit = () => {
+        store.logout();
+        window.location.reload();
+    };
+
+    if (!user) {
+        return (
+            <Grid container justifyContent="center">
+                <CircularProgress size={120} color="success" />
+            </Grid>
+        );
+    }
+
+    return (
+        <>
+            <Typography>Nickname: {user && user.name}</Typography>
+            <Typography>Email: {user && user.email}</Typography>
+            <Typography>Balance: {user && user.balance} $</Typography>
+            Own auction:
+            <ul>
+                {auctions.map(item => (
+                    <li key={item._id}>
+                        <a href={`/rate/${item._id}`}>{item.title}</a>
+                    </li>
+                ))}
+            </ul>
+            <Button variant="contained" onClick={handleSubmit} sx={{ mt: 3 }}>
+                Log out
+            </Button>
+        </>
+    );
+};
+
+export default UserIdPage;
