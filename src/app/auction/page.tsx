3fd07d94a514ead229bb7835bd4b0778729a346f
Kirill <93765186+Arsaide@users.noreply.github.com>
2024-05-02 15:21:09 +0300

    - Added hiding the form for creating an auction using a button

--- src/app/auction/page.tsx
+++ src/app/auction/page.tsx
@@ -1,26 +1,12 @@
 import React, { memo } from 'react';
-import CreateAuctionForm from '../../components/pages/auction/auctionForms/createAuction/CreateAuctionForm';
-import AuctionList from '../../components/pages/auction/auctionList/AuctionList';
-import useAuthCheck from '../../hooks/useAuthCheck/useAuthCheck';
-import InfoAlert from '../../components/layout/common/alerts/infoAlert/InfoAlert';
-import WarningAlert from '../../components/layout/common/alerts/warningAlert/WarningAlert';
+import './Page.css';
+import AuctionsPage from '../../components/pages/auction/AuctionsPage';
 
 const AuctionPage = () => {
-    const { isAuth } = useAuthCheck();
     return (
-        <section>
-            {isAuth ? (
-                <CreateAuctionForm />
-            ) : (
-                <WarningAlert
-                    title={'Why do I see this alert? - Log in, please'}
-                    text={
-                        'If you would like to post your auction, read more, or participate in the auction, please log in ✅'
-                    }
-                />
-            )}
-            <AuctionList />
-        </section>
+        <>
+            <AuctionsPage />
+        </>
     );
 };
 
---
+++ src/components/pages/auction/AuctionsPage.tsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import useAuthCheck from '../../../hooks/useAuthCheck/useAuthCheck';
+import Button from '@mui/material/Button';
+import { ButtonColors } from '../../../lib/Colors/ButtonColors';
+import { Close } from '@mui/icons-material';
+import CreateAuctionForm from './auctionForms/createAuction/CreateAuctionForm';
+import { Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+import WarningAlert from '../../layout/common/alerts/warningAlert/WarningAlert';
+import AuctionList from './auctionList/AuctionList';
+
+const AuctionsPage = () => {
+    const { isAuth } = useAuthCheck();
+    const [isFormVisible, setIsFormVisible] = useState<boolean>(false);
+
+    return (
+        <section>
+            {isAuth ? (
+                <>
+                    {isFormVisible ? (
+                        <>
+                            <Button
+                                variant="contained"
+                                sx={{
+                                    bgcolor: ButtonColors.LRED,
+                                    '&:hover': {
+                                        bgcolor: ButtonColors.DRED,
+                                    },
+                                    mb: 2,
+                                }}
+                                onClick={() => setIsFormVisible(false)}
+                            >
+                                <Close />
+                            </Button>
+                            <CreateAuctionForm />
+                        </>
+                    ) : (
+                        <>
+                            <Typography>
+                                You can create your own auction!
+                            </Typography>
+                            <Typography>
+                                Also you can read more about the functionality
+                                on the{' '}
+                                <Link to={'/information'} className={'link'}>
+                                    Information page
+                                </Link>
+                            </Typography>
+                            <Button
+                                variant="contained"
+                                sx={{
+                                    bgcolor: ButtonColors.LGREEN,
+                                    '&:hover': {
+                                        bgcolor: ButtonColors.DGREEN,
+                                    },
+                                }}
+                                onClick={() => setIsFormVisible(true)}
+                            >
+                                Create auction
+                            </Button>
+                        </>
+                    )}
+                </>
+            ) : (
+                <WarningAlert
+                    title={'Why do I see this alert? - Log in, please'}
+                    text={
+                        'If you would like to post your auction, read more, or participate in the auction, please log in ✅'
+                    }
+                />
+            )}
+            <AuctionList />
+        </section>
+    );
+};
+
+export default AuctionsPage;
