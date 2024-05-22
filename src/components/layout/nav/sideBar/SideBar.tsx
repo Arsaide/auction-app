13344eb6eae1b526059706043949c63c4f6ae1f5
Kirill <93765186+Arsaide@users.noreply.github.com>
2024-05-23 01:44:56 +0300

    - The SideBar menu has been reworked - Links have been changed, a bug with re-rendering the entire site when moving to another section has been fixed - Added active class to link - Optimizing your personal account path if getUser request pending - Changed styles in global.scss

--- src/components/layout/nav/sideBar/SideBar.tsx
+++ src/components/layout/nav/sideBar/SideBar.tsx
@@ -11,8 +11,18 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Toolbar from '@mui/material/Toolbar';
 import { MainColors } from '../../../../lib/colors/MainColors';
+import { useContext } from 'react';
+import { Context } from '../../../../index';
+import Avatar from '@mui/material/Avatar';
+import { AuthContext } from '../../../../lib/providers/AuthContext';
+import { NavLink } from 'react-router-dom';
+import './SideBar.css';
 
 const SideBar = () => {
+    const { store } = useContext(Context);
+    const { name, avatar, id } = store.user;
+    const { isLoggedIn, isLoading } = useContext(AuthContext);
+
     return (
         <>
             <Toolbar sx={{ bgcolor: MainColors.GRAY333 }} />
@@ -22,55 +32,93 @@ const SideBar = () => {
                     disablePadding
                     sx={{ '&:hover': { bgcolor: MainColors.GRAY333_01 } }}
                 >
-                    <ListItemButton href="/" sx={{ color: MainColors.WHITE }}>
-                        <ListItemIcon sx={{ color: MainColors.WHITE }}>
-                            <HomeIcon />
-                        </ListItemIcon>
-                        <ListItemText primary={'Home'} />
-                    </ListItemButton>
+                    <NavLink
+                        to={'/'}
+                        style={{ width: '100%' }}
+                        className={({ isActive }) =>
+                            [isActive ? 'active' : ''].join(' ')
+                        }
+                    >
+                        <ListItemButton sx={{ color: MainColors.WHITE }}>
+                            <ListItemIcon sx={{ color: MainColors.WHITE }}>
+                                <HomeIcon />
+                            </ListItemIcon>
+                            <ListItemText primary={'Home'} />
+                        </ListItemButton>
+                    </NavLink>
                 </ListItem>
                 <ListItem
                     disablePadding
                     sx={{ '&:hover': { bgcolor: MainColors.GRAY333_01 } }}
                 >
-                    <ListItemButton
-                        href="/auction"
-                        sx={{ color: MainColors.WHITE }}
-                    >
-                        <ListItemIcon sx={{ color: MainColors.WHITE }}>
-                            <CurrencyBitcoinIcon />
-                        </ListItemIcon>
-                        <ListItemText primary={'Place an auction'} />
-                    </ListItemButton>
+                    <NavLink to={'/auction'} style={{ width: '100%' }}>
+                        <ListItemButton sx={{ color: MainColors.WHITE }}>
+                            <ListItemIcon sx={{ color: MainColors.WHITE }}>
+                                <CurrencyBitcoinIcon />
+                            </ListItemIcon>
+                            <ListItemText primary={'Place an auction'} />
+                        </ListItemButton>
+                    </NavLink>
                 </ListItem>
                 <ListItem
                     disablePadding
                     sx={{ '&:hover': { bgcolor: MainColors.GRAY333_01 } }}
                 >
-                    <ListItemButton
-                        href="/online-chat"
-                        sx={{ color: MainColors.WHITE }}
-                    >
-                        <ListItemIcon sx={{ color: MainColors.WHITE }}>
-                            <QuestionAnswerIcon />
-                        </ListItemIcon>
-                        <ListItemText primary={'Online chat'} />
-                    </ListItemButton>
+                    <NavLink to={'/online-chat'} style={{ width: '100%' }}>
+                        <ListItemButton sx={{ color: MainColors.WHITE }}>
+                            <ListItemIcon sx={{ color: MainColors.WHITE }}>
+                                <QuestionAnswerIcon />
+                            </ListItemIcon>
+                            <ListItemText primary={'Online chat'} />
+                        </ListItemButton>
+                    </NavLink>
                 </ListItem>
                 <ListItem
                     disablePadding
                     sx={{ '&:hover': { bgcolor: MainColors.GRAY333_01 } }}
                 >
-                    <ListItemButton
-                        href="/information"
-                        sx={{ color: MainColors.WHITE }}
-                    >
-                        <ListItemIcon sx={{ color: MainColors.WHITE }}>
-                            <AssistantIcon />
-                        </ListItemIcon>
-                        <ListItemText primary={'Information'} />
-                    </ListItemButton>
+                    <NavLink to={'/information'} style={{ width: '100%' }}>
+                        <ListItemButton sx={{ color: MainColors.WHITE }}>
+                            <ListItemIcon sx={{ color: MainColors.WHITE }}>
+                                <AssistantIcon />
+                            </ListItemIcon>
+                            <ListItemText primary={'Information'} />
+                        </ListItemButton>
+                    </NavLink>
                 </ListItem>
+                {isLoggedIn && (
+                    <ListItem
+                        disablePadding
+                        sx={{
+                            '&:hover': { bgcolor: MainColors.GRAY333_01 },
+                            borderTop: 1,
+                            borderColor: 'divider',
+                            mt: 4,
+                        }}
+                    >
+                        <NavLink
+                            to={
+                                isLoading
+                                    ? `/personal-account`
+                                    : `/personal-account/${id}`
+                            }
+                            style={{ width: '100%' }}
+                        >
+                            <ListItemButton sx={{ color: MainColors.WHITE }}>
+                                <ListItemIcon sx={{ color: MainColors.WHITE }}>
+                                    <Avatar
+                                        sx={{ width: 24, height: 24 }}
+                                        alt={name}
+                                        src={`${avatar}`}
+                                    />
+                                </ListItemIcon>
+                                <ListItemText
+                                    primary={`${name || 'Personal'} account`}
+                                />
+                            </ListItemButton>
+                        </NavLink>
+                    </ListItem>
+                )}
             </List>
         </>
     );
